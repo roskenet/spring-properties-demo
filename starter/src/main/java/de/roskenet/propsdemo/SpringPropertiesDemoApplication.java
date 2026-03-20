149803f40ed37acb3863340256da809b897652e5
Felix Roske <felix@zalando.de>
2026-03-20 13:43:40 +0100

    Initial commit

---
+++ starter/src/main/java/de/roskenet/propsdemo/SpringPropertiesDemoApplication.java
@@ -0,0 +1,13 @@
+package de.roskenet.propsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPropertiesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPropertiesDemoApplication.class, args);
+    }
+
+}
---
+++ starter/src/test/java/de/roskenet/propsdemo/SpringPropertiesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.roskenet.propsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPropertiesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
